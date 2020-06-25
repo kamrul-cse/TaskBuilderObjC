a5836f9a3511a5f6e154cf880edc728e6ca8fea3
MKHG Lab <mkhglab@gmail.com>
2020-06-25 14:06:45 +0600

    basic flow done

---
+++ TaskBuilderObjC/Modules/Components/RowData/RowData.h
@@ -0,0 +1,16 @@
+//
+//  RowData.h
+//  TaskBuilderObjC
+//
+//  Created by Md. Kamrul Hasan on 24/6/20.
+//  Copyright © 2020 MKHG Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RowData: NSObject
+@property (strong, nonatomic) UITableViewCell* cell;
+@end
+
+@implementation RowData
+@end
